2c6154333c2c65048d732c08fb7ed00b8d57ff02
Christopher Loessl <cloessl+github@gmail.com>
2026-04-20 15:18:49 +0200

    feat: catch railway=halt subway stops in subway-broad query
    Some subway stations are tagged railway=halt + subway=yes instead of
    railway=station. Merge halt into the existing station regex to catch
    them in a single Overpass scan.

--- scripts/fetch-stations.fish
+++ scripts/fetch-stations.fish
@@ -25,7 +25,7 @@ set -l strip_delay 10
 set -l modes \
     'subway-nodes' 'node["station"="subway"];out body;' \
     'subway-all' 'nwr["station"="subway"];out body center;' \
-    'subway-broad' '(nwr["station"="subway"];nwr["railway"="station"]["subway"="yes"];);out body center;' \
+    'subway-broad' '(nwr["station"="subway"];nwr["railway"~"station|halt"]["subway"="yes"];);out body center;' \
     'all-rail' '(nwr["station"="subway"];nwr["railway"="station"]["subway"="yes"];nwr["station"~"light_rail|monorail"];nwr["railway"~"tram_stop|halt"];);out body center;' \
     'airports' 'nwr["aeroway"="aerodrome"];out body center;'
 
